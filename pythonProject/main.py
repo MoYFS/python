583e5354c02161bed25c51f06051446eb10e2e14
莫语浮生 <zhangjiarui2018@outlook.com>
2022-09-07 00:03:24 +0800

    python

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -6,7 +6,7 @@ for x in range(20):
     tl.circle(100,360)
     tl.left(18)
 tl.goto(200,2000)
-for x in range(20):
+for x in range(40):
     tl.shape("turtle")
     tl.circle(100,360)
-    tl.left(18)
+    tl.left(8)
