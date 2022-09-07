d503514cc9902a7e0a9753437bc4061378906585
莫语浮生 <zhangjiarui2018@outlook.com>
2022-09-07 14:19:21 +0800

    python

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,12 +1,15 @@
 import turtle as tl
+
 tl.color("red")
 tl.speed("fast")
 for x in range(20):
     tl.shape("turtle")
-    tl.circle(100,360)
+    tl.circle(100, 360)
     tl.left(18)
-tl.goto(200,2000)
-for x in range(20):
+tl.up()
+tl.goto(200, 200)
+tl.down()
+for x in range(40):
     tl.shape("turtle")
-    tl.circle(100,360)
-    tl.left(18)
+    tl.circle(100, 360)
+    tl.left(9)
