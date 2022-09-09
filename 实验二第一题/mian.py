8a014d088b8fc79b7cdc93135589b471e9632864
莫语浮生 <zhangjiarui2018@outlook.com>
2022-09-09 23:50:16 +0800

    作业

---
+++ 实验二第一题/mian.py
@@ -0,0 +1,33 @@
+"""
+revenue=98765
+costs=40000
+profit=revenue-costs
+print(profit)
+"""
+"""
+math=80
+chinese=85
+english=75
+print(" 总分：",english+chinese+math,'\n',"平均分：",(english+chinese+math)/3)
+"""
+"""
+import math
+radius=float(input())
+print(" 球的表面积为：%.4f"%(4*math.pi*radius*radius),'\n',"球的体积为：%.4f"%((4*math.pi*radius*radius*radius)/3))
+"""
+"""
+name=input()
+age=int(input())
+print("哇塞，",name,"今年",age,"岁",sep='')
+"""
+"""
+number=int(input())
+number1=int((number%10))*100+int(((number/10)%10))*10+int(number/100)
+print(number1)
+"""
+import math
+"""
+x=int(input())
+print(math.cos(x)*math.cos(x)+math.sin(x)*math.sin(x))
+"""
+print("x1=",(-2-math.sqrt(2*2-4*(-3)))/2,"x2=",(-2+math.sqrt(2*2-4*(-3)))/2)
