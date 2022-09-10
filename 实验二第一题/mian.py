f4a87d2ce4a2fa1c49b1a7e9703527997c9811cf
莫语浮生 <zhangjiarui2018@outlook.com>
2022-09-10 21:52:54 +0800

    作业

--- 实验二第一题/mian.py
+++ 实验二第一题/mian.py
@@ -1,33 +1,53 @@
+#第一题
 """
 revenue=98765
 costs=40000
 profit=revenue-costs
 print(profit)
 """
+#第二题
 """
 math=80
 chinese=85
 english=75
 print(" 总分：",english+chinese+math,'\n',"平均分：",(english+chinese+math)/3)
 """
+#第三题
 """
 import math
 radius=float(input())
 print(" 球的表面积为：%.4f"%(4*math.pi*radius*radius),'\n',"球的体积为：%.4f"%((4*math.pi*radius*radius*radius)/3))
 """
+#第四题
+"""
+name=input()
+print("姓名：",name)
+"""
+#第五题
+"""
+age=int(input())
+print(age+2)
+"""
+#第六题
 """
 name=input()
 age=int(input())
 print("哇塞，",name,"今年",age,"岁",sep='')
 """
+#第七题
 """
 number=int(input())
 number1=int((number%10))*100+int(((number/10)%10))*10+int(number/100)
 print(number1)
 """
-import math
+#第八题
 """
+import math
 x=int(input())
 print(math.cos(x)*math.cos(x)+math.sin(x)*math.sin(x))
 """
+#第九题
+import math
 print("x1=",(-2-math.sqrt(2*2-4*(-3)))/2,"x2=",(-2+math.sqrt(2*2-4*(-3)))/2)
+
+
