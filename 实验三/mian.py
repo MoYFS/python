9dd34db4608d8e8672d858006f868575995775ca
莫语浮生 <zhangjiarui2018@outlook.com>
2022-09-27 01:02:24 +0800

    实验三

--- 实验三/mian.py
+++ 实验三/mian.py
@@ -46,6 +46,17 @@ money=input("请输入要兑换的人民币币值，以￥结束：")
 money=float(money.replace('￥',''))
 print("{}元人民币可以兑换{:.2f}美元".format(money,money*0.146))
 '''
-
-
-
+'''
+dollar=input("请输入要兑换的美元币值，以$结束:")
+dollar=float(dollar.replace('$',''))
+print("{}美元可以兑换人民币{:.2f}元".format(dollar,dollar*6.868))
+'''
+money=input("请输入要兑换的货币币值，可以以¥或$结束：")
+symbol=money[-1];
+money=float(money[:-1:1])
+if symbol=='¥':
+    print("{}元人民币可以兑换{:.2f}美元".format(money,money*0.146))
+elif symbol=='$':
+    print("{}美元可以兑换人民币{:.2f}元".format(money,money*6.868))
+else:
+    print("无该{}汇率换算".format(symbol))
