f07cc4cc2ab2087ef7300680e29e72aacf1ce92d
莫语浮生 <zhangjiarui2018@outlook.com>
2022-10-15 22:07:30 +0800

    作业

--- 作业10月11号/main.py
+++ 作业10月11号/main.py
@@ -8,12 +8,30 @@ for i in range(1,n+1):
     s=s+a*temp
 print(s)
 '''
-cout=0
+
+'''
+count=0
 for x in range(101,201):
     for i in range(1,x):
         if x%i==0 and i!=1:
             break
     else:
-        cout+=1
+        count+=1
         print("{}是一个素数".format(x))
-print("一共有{}个素数".format(cout))
+print("一共有{}个素数".format(count))
+'''
+
+'''
+count=uneven=even=0
+while(1):
+    temp=input("输入整数，当输入A时结束：")
+    if temp=='A':
+        print("奇数和为{},偶数和为{}，平均数为{}".format(uneven,even,(even+uneven)/count))
+        break
+    else:
+        count+=1
+        if int(temp)%2==0:
+            even+=int(temp)
+        else:
+            uneven+=int(temp)
+'''
