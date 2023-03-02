bdf8f110f54ecc2b5847efb8d63f48552ff2c407
莫语浮生 <zhangjiarui2018@outlook.com>
2023-03-02 11:12:40 +0800

    海龟画图

---
+++ 海龟画图/main.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.setup(840,720)
+turtle.fillcolor("red")
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-135,50)
+turtle.pendown()
+turtle.begin_fill();
+#turtle.pencolor("red")
+# for i in range(5):
+#     turtle.forward(300)
+#     turtle.right(144)
+turtle.pencolor("black")
+for i in range(12):
+    turtle.forward(300)
+    turtle.right(150)
+turtle.end_fill()
+turtle.hideturtle()
+turtle.done()
