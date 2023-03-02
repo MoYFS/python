2b5a5396d39a0e58c874eb4fe2567dbcc3533e97
莫语浮生 <zhangjiarui2018@outlook.com>
2023-03-02 11:28:00 +0800

    圆形螺旋

--- 圆形螺旋/main.py
+++ 圆形螺旋/main.py
@@ -3,8 +3,8 @@ turtle.setup(840,840)
 turtle.speed(0)
 colors=["red","blue","green","yellow"]
 turtle.hideturtle()
-for i in range(100):
+for i in range(200):
     turtle.pencolor(colors[i%4])
-    turtle.circle(i*2)
+    turtle.circle(200-i)
     turtle.left(91)
 turtle.done()
