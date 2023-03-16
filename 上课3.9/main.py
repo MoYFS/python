0994b0fa4d9f7416024d62ccd237520d92555700
莫语浮生 <zhangjiarui2018@outlook.com>
2023-03-16 19:28:28 +0800

    词频统计

--- 上课3.9/main.py
+++ 上课3.9/main.py
@@ -4,5 +4,11 @@ n=int(sys.argv[1])
 for i in range(n):
     print(random.randrange(0,100))
 '''
+def return_values():
+    D={}
+    L=[5,[4,'myself'],(1,2,4),'learn']
+    for i ,m in zip(L,['a','b','c','d']):
+        D.setdefault(m,i)
+    return D
 
-import os
+print(return_values())
